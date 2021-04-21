32e92011fabc65fee1a1aa2ac97c89a7f7371bf3
MarcoMisiani <80473928+MarcoMisiani@users.noreply.github.com>
2021-04-21 14:57:32 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 echo "running test on cicd"
 
-cd /var/www/html/src/WebHookBundle
+cd /var/www/html/
 
 echo "working in html"
 
 pwd
-ls -l
+ls -l 
 
-composer 
-
-composer validate --strict --no-check-version
+composer validate --strict --no-check-version --working-dir /var/www/html/src/WebHookBundle
       
-composer install --prefer-dist --no-progress --ignore-platform-reqs --working-dir /var/www/html
+composer install --prefer-dist --no-progress --ignore-platform-reqs --working-dir /var/www/html/src/WebHookBundle
       
 vendor/bin/pimcore-install --admin-username pimcore --admin-password pimcore --mysql-username pimcore --mysql-password pimcore --mysql-database pimcore --mysql-host-socket db
 
