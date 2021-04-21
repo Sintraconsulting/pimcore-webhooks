5fbc9ba1ffa5cfdb133b167018da119070b2008d
MarcoMisiani <80473928+MarcoMisiani@users.noreply.github.com>
2021-04-21 15:01:18 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -11,6 +11,8 @@ ls -l
 composer validate --strict --no-check-version --working-dir /var/www/html/src/WebHookBundle
       
 composer install --prefer-dist --no-progress --ignore-platform-reqs --working-dir /var/www/html/src/WebHookBundle
+
+ls -l 
       
 vendor/bin/pimcore-install --admin-username pimcore --admin-password pimcore --mysql-username pimcore --mysql-password pimcore --mysql-database pimcore --mysql-host-socket db
 
