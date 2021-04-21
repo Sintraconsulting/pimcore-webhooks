d036bb3c64aa3760167dc1df3a546b5969f24c3d
MarcoMisiani <80473928+MarcoMisiani@users.noreply.github.com>
2021-04-21 13:03:45 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -17,7 +17,7 @@ ls -l
       
 /vendor/bin/pimcore-install --admin-username pimcore --admin-password pimcore --mysql-username pimcore --mysql-password pimcore --mysql-database pimcore --mysql-host-socket db
 
-cd tests
-/var/www/html/vendor/bin/simple-phpunit /  --filter testB WebHookTest.php
+cd /src/WebHookBundle/tests
+/var/www/html/vendor/bin/simple-phpunit --filter testB WebHookTest.php
 
 
