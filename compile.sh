0672f98e5cd4d7bf09eb1af72a11e74123f133f1
MarcoMisiani <80473928+MarcoMisiani@users.noreply.github.com>
2021-04-21 13:07:58 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -10,12 +10,12 @@ ls -l
 
 src/WebHookBundle/composer validate --strict --no-check-version
       
-composer install --prefer-dist --no-progress --ignore-platform-reqs
+src/WebHookBundle/composer install --prefer-dist --no-progress --ignore-platform-reqs
 
 pwd
 ls -l
       
-/vendor/bin/pimcore-install --admin-username pimcore --admin-password pimcore --mysql-username pimcore --mysql-password pimcore --mysql-database pimcore --mysql-host-socket db
+vendor/bin/pimcore-install --admin-username pimcore --admin-password pimcore --mysql-username pimcore --mysql-password pimcore --mysql-database pimcore --mysql-host-socket db
 
 cd src/WebHookBundle/tests
 /var/www/html/vendor/bin/simple-phpunit --filter testB WebHookTest.php
