5d0840a9e9efde63bacd87ef012aa5cd86be1001
MarcoMisiani <80473928+MarcoMisiani@users.noreply.github.com>
2021-04-21 15:46:10 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,25 +1,21 @@
 #!/bin/bash
 echo "running test on cicd"
 
-cd /var/www/html/src/WebHookBundle
+cd /var/www/html/
 
 echo "working in html"
 
+mv src/WebHookBundle/composer.json ./
 pwd
 ls -l 
 
-composer validate --strict --no-check-version --working-dir /var/www/html
+composer validate --strict --no-check-version --working-dir
       
-composer install --prefer-dist --no-progress --ignore-platform-reqs --working-dir /var/www/html
+composer install --prefer-dist --no-progress --ignore-platform-reqs
 
-ls -l 
-echo "cambio dir"
-cd /var/www/html
-ls -l 
-      
 vendor/bin/pimcore-install --admin-username pimcore --admin-password pimcore --mysql-username pimcore --mysql-password pimcore --mysql-database pimcore --mysql-host-socket db
 
-cd tests
+cd src/WebHookBundle/tests
 /var/www/html/vendor/bin/simple-phpunit --filter testB WebHookTest.php
 
 
