2023998c857ab926e7a0cdd319a756ae7685ad72
MarcoMisiani <80473928+MarcoMisiani@users.noreply.github.com>
2021-04-21 12:19:57 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 echo "running test on cicd"
-cd /var/www/html/tests
-pwd
-ls -l
+
+
+composer validate --strict --no-check-version
+      
+composer install --prefer-dist --no-progress --ignore-platform-reqs
+      
+/vendor/bin/pimcore-install --admin-username pimcore --admin-password pimcore --mysql-username pimcore --mysql-password pimcore --mysql-database pimcore --mysql-host-socket db
+
 /var/www/html/vendor/bin/simple-phpunit --filter testB WebHookTest.php
 
 
