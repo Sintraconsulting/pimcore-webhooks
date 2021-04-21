f648b880e3b4deb08ae1dac61ca38dbbb3b11dbd
MarcoMisiani <80473928+MarcoMisiani@users.noreply.github.com>
2021-04-21 17:31:00 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -12,7 +12,8 @@ ls -l
 composer validate --strict --no-check-version
       
 composer install --prefer-dist --no-progress --ignore-platform-reqs
-
+pwd
+ls -l 
 vendor/bin/pimcore-install --admin-username pimcore --admin-password pimcore --mysql-username pimcore --mysql-password pimcore --mysql-database pimcore --mysql-host-socket db
 
 
