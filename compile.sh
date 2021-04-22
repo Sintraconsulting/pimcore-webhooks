14bcae66a9ca727f6e1f4b0ccb68cea1f3870be8
MarcoMisiani <80473928+MarcoMisiani@users.noreply.github.com>
2021-04-22 14:48:41 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,13 +5,18 @@ cd /var/www/html/
 
 echo "working in html"
 
-mv src/WebHookBundle/composer.json ./
+COMPOSER_MEMORY_LIMIT=-1 composer create-project pimcore/skeleton tmp
+mv tmp/.[!.]* .
+mv tmp/* .
+rmdir tmp
+
 pwd
 ls -l 
 
-composer validate --strict --no-check-version
+composer validate --strict --no-check-version 
       
 composer install --prefer-dist --no-progress --ignore-platform-reqs
+
 pwd
 ls -l 
 vendor/bin/pimcore-install --admin-username pimcore --admin-password pimcore --mysql-username pimcore --mysql-password pimcore --mysql-database pimcore --mysql-host-socket db
@@ -19,8 +24,11 @@ vendor/bin/pimcore-install --admin-username pimcore --admin-password pimcore --m
 pwd
 ls -l 
 
-cd src/WebHookBundle/tests/
+cd src/WebHookBundle/
 
+composer validate --strict --no-check-version 
+
+cd tests
 
 pwd
 ls -l
